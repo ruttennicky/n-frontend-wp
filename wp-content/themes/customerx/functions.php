e0c1aa2bc12eb596ca0f0fa4e0ff8ff42d4ec195
Nicky Rutten <nicky.rutten@acquia.com>
2018-01-05 16:21:43 +0000

    refined the child theme functions

--- wp-content/themes/customerx/functions.php
+++ wp-content/themes/customerx/functions.php
@@ -1,12 +1,16 @@
 <?php
   function load_custom_js() {
+    if(!is_admin()) {
 
-    wp_enqueue_script('custom-scripts', get_stylesheet_directory_uri() . '/js/custom.js',array('jquery'),false,true);
+      wp_enqueue_script('custom-scripts', get_stylesheet_directory_uri() . '/js/custom.js',array('jquery'),false,true);
 
+    }
   }
 
   function load_custom_css() {
+    if(!is_admin()) {
 
+    }
   }
 
   add_action('wp_enqueue_scripts','load_custom_js');
